1fb28ce76cdb28dc23e280ea6278de798a445cb8
Arthur Soares <arthurvsp97@gmail.com>
2020-06-14 16:00:06 -0300

    Create react-app

---
+++ build/precache-manifest.f7c2d49b7b5d56a0841794e869e769cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bde118418948e1d62a2dc78ffba37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42ec216dab18919b38c2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "098c816c3e31084065f5",
+    "url": "/static/js/2.30506d40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30506d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ec216dab18919b38c2",
+    "url": "/static/js/main.3fa5a5b3.chunk.js"
+  },
+  {
+    "revision": "ab38467fb9741f6428a3",
+    "url": "/static/js/runtime-main.c11ca102.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

f321914acd1cb04ebacd03329721154d457d7309
Arthur Soares <arthurvsp97@gmail.com>
2020-07-12 17:53:18 -0300

    Updates

---
+++ precache-manifest.1f3b2b87268d2c649ca3bfc27eb289da.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e624384b525d65b6ae228a794a4b7574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9147fdd017e187683753",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f492727104f381b343cd",
+    "url": "/static/css/main.449ca3e3.chunk.css"
+  },
+  {
+    "revision": "9147fdd017e187683753",
+    "url": "/static/js/2.9edb5201.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9edb5201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f492727104f381b343cd",
+    "url": "/static/js/main.9a613510.chunk.js"
+  },
+  {
+    "revision": "13579146213f330cbc10",
+    "url": "/static/js/runtime-main.b0a82034.js"
+  },
+  {
+    "revision": "15823c9952df86b7f1fed5cc2f829f18",
+    "url": "/static/media/bootstrap-logo.15823c99.png"
+  },
+  {
+    "revision": "e63574ef69efa791508ee40847da9ac4",
+    "url": "/static/media/me.e63574ef.png"
+  },
+  {
+    "revision": "da98f7332c8fc34512cb9c85afdb69fd",
+    "url": "/static/media/msf-logo.da98f733.png"
+  },
+  {
+    "revision": "254e2aade1a20e80766f37f209f25845",
+    "url": "/static/media/react-logo.254e2aad.png"
+  },
+  {
+    "revision": "05b3f0067045886c31af9107b7d0c40b",
+    "url": "/static/media/react-router-logo.05b3f006.png"
+  },
+  {
+    "revision": "18ea02d217e41485bd0513d64b74237d",
+    "url": "/static/media/typescript-logo.18ea02d2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9bb18994a1a4177904b69d036516880.js"
+  "/precache-manifest.1f3b2b87268d2c649ca3bfc27eb289da.js"
 );
 
 self.addEventListener('message', (event) => {

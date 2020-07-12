c7f6134593bf422935ae94639a43dc2b94cfb810
Arthur Soares <arthurvsp97@gmail.com>
2020-07-12 16:03:36 -0300

    Adding margin on top of footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,8 +4,10 @@ import Container from 'react-bootstrap/Container';
 export default class Footer extends Component {
   render() {
     return (
-      <Container fluid className="b--light-silver bt w-100 tc">
-        <p className="mb2 mt2 silver f6">Copyright © 2020 Arthur Soares. Todos os Direitos Reservados</p>
+      <Container fluid className="b--light-silver bt w-100 tc mt3">
+        <p className="mb2 mt2 silver f6">
+          Copyright © 2020 Arthur Soares. Todos os Direitos Reservados
+        </p>
       </Container>
     );
   };
